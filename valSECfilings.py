e8d2db48317563f4d8aa93105fbb79badfe79e64
edi <edi.first@.altova.com>
2015-07-31 17:00:31 +0200

    Support for RaptorXML+XBRL releases equal or newer than v2015r3

--- valSECfilings.py
+++ valSECfilings.py
@@ -1,11 +1,11 @@
 # Copyright 2014 Altova GmbH
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -95,7 +95,7 @@ def validatejob( joblist, script=None ):
 	if script==None:
 		result = call(["raptorxmlxbrl", "xbrl", "--listfile", joblist] )	#  "--verbose=true",
 	else:
-		result = call(["raptorxmlxbrl", "xbrl", "--script="+script,
+		result = call(["raptorxmlxbrl", "xbrl", "--script-api-version=1", "--script="+script,
 						"--listfile", joblist] )
 	return result
 
